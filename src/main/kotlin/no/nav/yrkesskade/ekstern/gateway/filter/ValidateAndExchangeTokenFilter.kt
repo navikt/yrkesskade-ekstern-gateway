73af9da2e0b478a2ee9a6433394e3243b844d37e
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2022-04-19 10:48:17 +0200

    Dynamisk henting av jwt klient ut fra hvilken route som matchet

--- src/main/kotlin/no/nav/yrkesskade/ekstern/gateway/filter/ValidateAndExchangeTokenFilter.kt
+++ src/main/kotlin/no/nav/yrkesskade/ekstern/gateway/filter/ValidateAndExchangeTokenFilter.kt
@@ -5,12 +5,16 @@ import no.nav.yrkesskade.ekstern.gateway.tokenx.TokenXClient
 import org.slf4j.LoggerFactory
 import org.springframework.cloud.gateway.filter.GatewayFilterChain
 import org.springframework.cloud.gateway.filter.GlobalFilter
+import org.springframework.cloud.gateway.route.Route
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Mono
 import java.lang.invoke.MethodHandles
+
+private const val MASKINPORTEN = "maskinporten"
 
 /**
  * kul beskrivelse
@@ -21,7 +25,6 @@ class ValidateAndExchangeTokenFilter(
     val tokenXClient: TokenXClient
 ) : GlobalFilter {
     val logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass())
-
     val validationHandler = JwtTokenValidationHandler(config)
 
     override fun filter(exchange: ServerWebExchange?, chain: GatewayFilterChain?): Mono<Void> {
@@ -29,11 +32,11 @@ class ValidateAndExchangeTokenFilter(
         val validatedTokens = validationHandler.getValidatedTokens(exchange.request)
 
         if (validatedTokens.hasValidToken()) {
-            logger.info(validatedTokens.getJwtToken("maskinporten").tokenAsString)
+            logger.info(validatedTokens.getJwtToken(MASKINPORTEN).tokenAsString)
 
             val exchangedToken = tokenXClient.exchange(
-                validatedTokens.getJwtToken("maskinporten").tokenAsString,
-                "yrkesskade-melding-api"
+                validatedTokens.getJwtToken(MASKINPORTEN).tokenAsString,
+                resolveJwtClient(exchange)
             )
 
             exchange.request.mutate()
@@ -45,5 +48,10 @@ class ValidateAndExchangeTokenFilter(
             }.setComplete()
         }
         return chain!!.filter(exchange)
+    }
+
+    private fun resolveJwtClient(exchange: ServerWebExchange): String {
+        val route: Route = exchange.attributes[ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR] as Route
+        return route.id
     }
 }
