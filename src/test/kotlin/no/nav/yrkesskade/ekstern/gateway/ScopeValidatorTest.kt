8f91431098b6a7ef413e43814871db57e47efb6e
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2022-04-25 14:41:16 +0200

    YSMOD-205 * Bruke wiremock for å mocke token endpoint i test * Formatering

--- src/test/kotlin/no/nav/yrkesskade/ekstern/gateway/ScopeValidatorTest.kt
+++ src/test/kotlin/no/nav/yrkesskade/ekstern/gateway/ScopeValidatorTest.kt
@@ -1,6 +1,6 @@
 package no.nav.yrkesskade.ekstern.gateway
 
-import no.nav.yrkesskade.ekstern.gateway.config.ScopeValidationConfiguration
+import no.nav.yrkesskade.ekstern.gateway.mock.TestMockServerInitialization
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.web.server.ServerWebExchange
@@ -11,101 +11,100 @@ import org.springframework.cloud.gateway.support.ServerWebExchangeUtils
 import org.springframework.mock.http.server.reactive.MockServerHttpRequest
 import org.springframework.mock.web.server.MockServerWebExchange
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
 
 private const val HTTP_LOCALHOST_8080 = "http://localhost:8080"
 
 @ActiveProfiles("test")
 @SpringBootTest
+@ContextConfiguration(initializers = [TestMockServerInitialization::class])
 internal class ScopeValidatorTest {
 
     @Autowired
     lateinit var scopeValidator: ScopeValidator
 
-    @Autowired
-    lateinit var scopeValidationConfiguration: ScopeValidationConfiguration
-
     @Test
     fun `validateScope - correct scope for kodeverk yields true`() {
         val result = scopeValidator.validateScope(kodeverkExchange(), "nav:yrkesskade:kodeverk.read")
-        Assertions.assertThat(result).isTrue()
+        Assertions.assertThat(result).isTrue
     }
 
     @Test
     fun `validateScope - non-matching scope for kodeverk yields false`() {
         val result = scopeValidator.validateScope(kodeverkExchange(), "nav:yrkesskade:skademelding.write")
-        Assertions.assertThat(result).isFalse()
+        Assertions.assertThat(result).isFalse
     }
 
     @Test
     fun `validateScope - nonexistent scope for kodeverk yields false`() {
         val result = scopeValidator.validateScope(kodeverkExchange(), null)
-        Assertions.assertThat(result).isFalse()
+        Assertions.assertThat(result).isFalse
     }
 
     @Test
     fun `validateScope - correct scope for skademelding yields true`() {
         val result = scopeValidator.validateScope(skademeldingExchange(), "nav:yrkesskade:skademelding.write")
-        Assertions.assertThat(result).isTrue()
+        Assertions.assertThat(result).isTrue
     }
 
     @Test
     fun `validateScope - incorrect scope for skademelding yields false`() {
         val result = scopeValidator.validateScope(skademeldingExchange(), "nav:yrkesskade:skattemelding.write")
-        Assertions.assertThat(result).isFalse()
+        Assertions.assertThat(result).isFalse
     }
 
     @Test
     fun `validateScope - nonexistent scope for skademelding yields false`() {
         val result = scopeValidator.validateScope(skademeldingExchange(), null)
-        Assertions.assertThat(result).isFalse()
+        Assertions.assertThat(result).isFalse
     }
 
     @Test
     fun `validateScope - any scope for apidoc yields true`() {
         val result = scopeValidator.validateScope(apidocExchange(), "any scope")
-        Assertions.assertThat(result).isTrue()
+        Assertions.assertThat(result).isTrue
     }
 
     @Test
     fun `validateScope - kodeverk scope for apidoc yields true`() {
         val result = scopeValidator.validateScope(apidocExchange(), "nav:yrkesskade:kodeverk.read")
-        Assertions.assertThat(result).isTrue()
+        Assertions.assertThat(result).isTrue
     }
 
     @Test
     fun `validateScope - nonexistent scope for apidoc yields true`() {
         val result = scopeValidator.validateScope(apidocExchange(), null)
-        Assertions.assertThat(result).isTrue()
+        Assertions.assertThat(result).isTrue
     }
 
     @Test
     fun `validateScope - empty scope for apidoc yields true`() {
         val result = scopeValidator.validateScope(apidocExchange(), "")
-        Assertions.assertThat(result).isTrue()
+        Assertions.assertThat(result).isTrue
     }
 
     @Test
     fun `validateScope - any scope for swagger yields true`() {
         val result = scopeValidator.validateScope(swaggerExchange(), "any scope")
-        Assertions.assertThat(result).isTrue()
+        Assertions.assertThat(result).isTrue
     }
 
     @Test
     fun `validateScope - kodeverk scope for swagger yields true`() {
         val result = scopeValidator.validateScope(swaggerExchange(), "nav:yrkesskade:kodeverk.read")
-        Assertions.assertThat(result).isTrue()
+        Assertions.assertThat(result).isTrue
     }
 
     @Test
     fun `validateScope - nonexistent scope for swagger yields true`() {
         val result = scopeValidator.validateScope(swaggerExchange(), null)
-        Assertions.assertThat(result).isTrue()
+        Assertions.assertThat(result).isTrue
     }
 
     @Test
     fun `validateScope - empty scope for swagger yields true`() {
         val result = scopeValidator.validateScope(swaggerExchange(), "")
-        Assertions.assertThat(result).isTrue()
+        Assertions.assertThat(result).isTrue
     }
 
     fun kodeverkExchange(): ServerWebExchange {
@@ -114,7 +113,7 @@ internal class ScopeValidatorTest {
             this.attributes[ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR] = Route.async()
                 .id("yrkesskade-kodeverk")
                 .uri(HTTP_LOCALHOST_8080)
-                .predicate { exp -> true }
+                .predicate { true }
                 .build()
         }
         return exchange
@@ -140,7 +139,7 @@ internal class ScopeValidatorTest {
             this.attributes[ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR] = Route.async()
                 .id("yrkesskade-melding-api")
                 .uri(HTTP_LOCALHOST_8080)
-                .predicate { exp -> true }
+                .predicate { true }
                 .build()
         }
     }
