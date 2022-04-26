4c9428e59b5e757d6989b68bbcaefb7204ab3e4a
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2022-04-26 13:46:50 +0200

    YSMOD-205 * Flyttet testoppsettsdata til fixtures.kt * Lagt til mocktest for scenariene som kaller (og ikke kaller) på TokenX

--- src/test/kotlin/no/nav/yrkesskade/ekstern/gateway/ScopeValidatorTest.kt
+++ src/test/kotlin/no/nav/yrkesskade/ekstern/gateway/ScopeValidatorTest.kt
@@ -13,8 +13,6 @@ import org.springframework.mock.web.server.MockServerWebExchange
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 
-private const val HTTP_LOCALHOST_8080 = "http://localhost:8080"
-
 @ActiveProfiles("test")
 @SpringBootTest
 @ContextConfiguration(initializers = [TestMockServerInitialization::class])
@@ -105,42 +103,5 @@ internal class ScopeValidatorTest {
     fun `validateScope - empty scope for swagger yields true`() {
         val result = scopeValidator.validateScope(swaggerExchange(), "")
         Assertions.assertThat(result).isTrue
-    }
-
-    fun kodeverkExchange(): ServerWebExchange {
-        val request = MockServerHttpRequest.get("$HTTP_LOCALHOST_8080/api/v1/kodeverk/typer")
-        val exchange: ServerWebExchange = MockServerWebExchange.from(request).apply {
-            this.attributes[ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR] = Route.async()
-                .id("yrkesskade-kodeverk")
-                .uri(HTTP_LOCALHOST_8080)
-                .predicate { true }
-                .build()
-        }
-        return exchange
-    }
-
-    fun skademeldingExchange(): ServerWebExchange {
-        val request = MockServerHttpRequest.post("$HTTP_LOCALHOST_8080/api/v1/skademeldinger").build()
-        return yrkesskadeMeldingApiExchange(request)
-    }
-
-    fun apidocExchange(): ServerWebExchange {
-        val request = MockServerHttpRequest.get("$HTTP_LOCALHOST_8080/api/v3/api-doc").build()
-        return yrkesskadeMeldingApiExchange(request)
-    }
-
-    fun swaggerExchange(): ServerWebExchange {
-        val request = MockServerHttpRequest.get("$HTTP_LOCALHOST_8080/api/swagger-ui/index.html").build()
-        return yrkesskadeMeldingApiExchange(request)
-    }
-
-    fun yrkesskadeMeldingApiExchange(request: MockServerHttpRequest): ServerWebExchange {
-        return MockServerWebExchange.from(request).apply {
-            this.attributes[ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR] = Route.async()
-                .id("yrkesskade-melding-api")
-                .uri(HTTP_LOCALHOST_8080)
-                .predicate { true }
-                .build()
-        }
     }
 }
