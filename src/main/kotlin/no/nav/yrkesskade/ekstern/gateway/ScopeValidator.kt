edd21d9c335b1cbac046ca1c7f1323678b21aec2
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2022-04-27 10:48:37 +0200

    YSMOD-205 Div javadoc og kommentarer

--- src/main/kotlin/no/nav/yrkesskade/ekstern/gateway/ScopeValidator.kt
+++ src/main/kotlin/no/nav/yrkesskade/ekstern/gateway/ScopeValidator.kt
@@ -8,13 +8,33 @@ import org.springframework.web.server.ServerWebExchange
 @Component
 class ScopeValidator(val scopeValidationConfiguration: ScopeValidationConfiguration) {
 
+    /**
+     * Sjekker at scopet som ligger på tokenet er gyldig gitt pathen som ligger på requestet.
+     * [scopeValidationConfiguration] inneholder en liste over hvilke scopes som er gyldige per path.
+     * Hvert innslag i denne scope-lista har en nøkkel som må stemme overens med route id.
+     * F.eks.:
+     * <code>
+     *     ...
+     *      routes:
+     *          - id: yrkesskade-melding-api
+     *      ...
+     *      scopes:
+     *          - name: yrkesskade-melding-api
+     *   </code>
+     *
+     * @param exchange request-response interaksjonen vi befinner oss i
+     * @param scopeFromToken scopet fra Maskinporten-tokenet fra requestet
+     */
     fun validateScope(exchange: ServerWebExchange, scopeFromToken: String?): Boolean {
         val scopesForRoute = scopeValidationConfiguration.scopes[exchange.getRouteId()]
+
+        // Finn scope-navnet som tilhører den konkrete pathen som ligger på requestet
         val matchingScope = scopesForRoute?.find { route ->
             AntPathMatcher().match(route.path, exchange.request.path.toString())
         }
 
         if (matchingScope?.name.isNullOrBlank()) {
+            // ikke definert noe scope for den aktuelle pathen
             return false
         }
         if (matchingScope?.acceptsAnyScope() == true) {
--- src/main/kotlin/no/nav/yrkesskade/ekstern/gateway/config/ScopeValidationConfiguration.kt
+++ src/main/kotlin/no/nav/yrkesskade/ekstern/gateway/config/ScopeValidationConfiguration.kt
@@ -3,6 +3,23 @@ package no.nav.yrkesskade.ekstern.gateway.config
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 
+/**
+ * En klasse som inneholder alle scopes som er definert for hver path, innenfor hver route id.
+ * Hvert innslag i denne scope-lista har en nøkkel som må stemme overens med route id.
+ * F.eks.:
+ * <code>
+ *     ...
+ *      routes:
+ *          - id: yrkesskade-melding-api
+ *      ...
+ *      scopes:
+ *          - name: yrkesskade-melding-api
+ *   </code>
+ *
+ * Pathene følger Ant-style patterns.
+ * Vi er nødt til å eksplisitt definere hvilke pather som ikke krever noe spesifikt scope. Dette er løst ved å gi disse
+ * scopene en prefix ved navn "noscope".
+ */
 @ConfigurationProperties("token.validation")
 @ConstructorBinding
 class ScopeValidationConfiguration(val scopes: Map<String, List<ScopeValidationProperties>>)
