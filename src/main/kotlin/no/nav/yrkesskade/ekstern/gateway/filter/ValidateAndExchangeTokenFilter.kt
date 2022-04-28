530806b2f210ddf6038f6c0edc882cb4d3c40cf2
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2022-04-28 13:07:57 +0200

    YSMOD-205 Litt mer logging

--- src/main/kotlin/no/nav/yrkesskade/ekstern/gateway/filter/ValidateAndExchangeTokenFilter.kt
+++ src/main/kotlin/no/nav/yrkesskade/ekstern/gateway/filter/ValidateAndExchangeTokenFilter.kt
@@ -6,6 +6,7 @@ import no.nav.yrkesskade.ekstern.gateway.config.ScopeValidationConfiguration
 import no.nav.yrkesskade.ekstern.gateway.config.TokenXClientListProperties
 import no.nav.yrkesskade.ekstern.gateway.getRouteId
 import no.nav.yrkesskade.ekstern.gateway.tokenx.TokenXClient
+import org.slf4j.LoggerFactory
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.gateway.filter.GatewayFilterChain
 import org.springframework.cloud.gateway.filter.GlobalFilter
@@ -16,6 +17,7 @@ import org.springframework.http.server.reactive.ServerHttpResponse
 import org.springframework.stereotype.Component
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Mono
+import java.lang.invoke.MethodHandles
 
 private const val MASKINPORTEN = "maskinporten"
 
@@ -35,19 +37,23 @@ class ValidateAndExchangeTokenFilter(
     val scopeValidator: ScopeValidator,
     val tokenXClientListProperties: TokenXClientListProperties
 ) : GlobalFilter {
+    val logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass())
 
     override fun filter(exchange: ServerWebExchange?, chain: GatewayFilterChain?): Mono<Void> {
         val response = exchange!!.response
         val validatedTokens = validationHandler.getValidatedTokens(exchange.request)
 
         if (!validatedTokens.hasValidToken()) {
+            logger.info("Ingen gyldige tokens")
             return errorResponse(response, HttpStatus.UNAUTHORIZED)
         }
 
         val maskinportenToken = validatedTokens.getJwtToken(MASKINPORTEN)
 
-        val validScope = scopeValidator.validateScope(exchange, maskinportenToken.jwtTokenClaims.get("scope") as String?)
+        val scopeFromToken = maskinportenToken.jwtTokenClaims.get("scope") as String?
+        val validScope = scopeValidator.validateScope(exchange, scopeFromToken)
         if (!validScope) {
+            logger.info("Ugyldig scope $scopeFromToken for path ${exchange.request.path}")
             return errorResponse(response, HttpStatus.FORBIDDEN)
         }
 
@@ -62,6 +68,7 @@ class ValidateAndExchangeTokenFilter(
     private fun exchangeTokenIfNecessary(maskinportenToken: JwtToken, exchange: ServerWebExchange) {
         val client = exchange.getRouteId()
         if (!tokenXClientListProperties.clientList.contains(client)) {
+            logger.info("Ingen treff i TokenX-lista for klient $client. Utfører ikke tokenX.")
             return
         }
 
@@ -69,6 +76,7 @@ class ValidateAndExchangeTokenFilter(
         exchange.request.mutate()
             .header(HttpHeaders.AUTHORIZATION, "Bearer $exchangedToken")
             .build()
+        logger.info("Utført tokenX")
     }
 
     private fun errorResponse(response: ServerHttpResponse, status: HttpStatus): Mono<Void> {
